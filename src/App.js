9ef60bcba810591cbf45c221947f68620ede5b28
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-09 13:55:46 -0400

    add Nav componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from './componets/Nav';
 import About from './componets/about';
 
 function App() {
 
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
