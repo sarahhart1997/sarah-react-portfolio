7975f8d28abd1d587c18c5139555aab5cfc0e4b9
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-09 13:06:30 -0400

    updates

---
+++ src/componets/about/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <h1 id="about">A little bit about me...</h1>
+        </section>
+    );
+}
+
+export default About;
